563950386d1834880a503319766d7483be53187b
Olga <olga.dethlefsen@nbis.se>
2025-11-03 20:19:49 +0100

    Change icon depending on the weather

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+function formatTime(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function formatDate(date) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let weekday = days[date.getDay()];
+  let month = months[date.getMonth()];
+  let monthday = date.getDate();
+
+  return `${weekday}, ${month} ${monthday}`;
+}
+
 function refreshWeather(response) {
   // temperature and weather element
   let tempElement = document.querySelector("#temp");
@@ -17,11 +61,23 @@ function refreshWeather(response) {
   let wind = Number.parseFloat(response.data.wind.speed).toFixed(1);
   let tempFeelsLike = Math.round(response.data.temperature.feels_like);
 
+  //  date and time
+  let date = new Date(response.data.time * 1000);
+  let currentTimeElement = document.querySelector("#current-time");
+  let currentDateElement = document.querySelector("#current-date");
+
+  // icon
+  let iconElement = document.querySelector("#temp-icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
   tempElement.innerHTML = temp;
   currentDescriptionElement.innerHTML = currentDescription;
   humidityElement.innerHTML = humiditiy;
   windElement.innerHTML = wind;
   feelsLikeTempElement.innerHTML = tempFeelsLike;
+
+  currentTimeElement.innerHTML = formatTime(date);
+  currentDateElement.innerHTML = formatDate(date);
 }
 
 function searchCity(city) {
