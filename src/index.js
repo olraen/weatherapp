4f6f897cc60e048dff3c09990d9dc67fce6f803c
Olga <olga.dethlefsen@nbis.se>
2025-11-03 20:24:00 +0100

    Clean up, remove testing bits

--- src/index.js
+++ src/index.js
@@ -102,18 +102,3 @@ function handleSearchForm(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchForm);
-
-let query = "London";
-let apiKey = "1o02ad6aa40e7b19fa37f8t5928ba37d";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
-
-axios.get(apiUrl);
-
-axios
-  .get(apiUrl)
-  .then((response) => {
-    console.log(response.data); // 👈 This shows what was fetched
-  })
-  .catch((error) => {
-    console.error(error);
-  });
